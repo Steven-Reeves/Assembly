953bd1d355a5e27979ec2f07c6645ceb205f1f54
steven-reeves <stevenreeves15@gmail.com>
2018-05-18 19:54:34 -0700

    Added final changes to lab 4

--- lab4.X/main3.c
+++ lab4.X/main3.c
@@ -7,7 +7,8 @@ void main(void)
     int i = 10;
     int j = 20;
 
-    i = var2();
+    func1 (1,5);
+    //i = var2();
     j = i + 1;
 
 }
@@ -17,17 +18,26 @@ int var2(void)
     int i = 10;
     int j = 20;
 
-    i = var3(1, 2) + j;
+    //i = var3(1, 2) + j;
     return (i);
 }
 
-int var3(int l, int m)
+int func1(int ARG1, int ARG2)
 {
-    int i = 10;
-    int j = 20;
-    int k = 30;
+    int LOC1 = 10;
+    int LOC2 = 20;
+    int LOC3 = 30;
+    int x = func2(1,2,3,4,5);
+        //k = i + j + l + m;
 
-        k = i + j + l + m;
+    return (LOC3);
+}
 
-    return (k);
+int func2(int ARG1, int ARG2,int ARG3, int ARG4, int ARG5)
+{
+    int LOC1 = 10;
+    int LOC2 = 20;
+    int LOC3 = 30;
+
+    return 42;
 }
